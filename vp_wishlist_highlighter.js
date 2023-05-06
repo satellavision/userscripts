dd7a8b335a6ad3e8d036227a8db336dd4265ed5e
unknown <satellavision@gmail.com>
2023-05-06 22:02:58 +0100

    made wishlist highlighter more consistent

--- vp_wishlist_highlighter.js
+++ vp_wishlist_highlighter.js
@@ -1,67 +1,61 @@
 // ==UserScript==
 // @name        wishlist highlighter for virtu.pet
 // @match       https://virtu.pet/*
-// @version     1.0
+// @version     0.1
 // @author      satella
+// @grant       GM.setValue
+// @grant       GM.getValue
 // ==/UserScript==
 
-(function() {
+(async function() {
   'use strict';
 
-  function getWishlistItems() {
-    const wishlistUrl = 'https://virtu.pet/wishlist';
-    return fetch(wishlistUrl)
-      .then(response => response.text())
-      .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const mb1Elements = doc.querySelectorAll('.mb-1');
+  const username = document.querySelector('#username').textContent.trim();
+  const wishlistUrl = `https://virtu.pet/wishlist/${username}`;
 
-        // the first mb-1 element is the nst time, hence the slice(1)
-        return Array.from(mb1Elements).map(elem => elem.textContent.trim()).slice(1);
-      })
-      .catch(error => {
-        console.error('Error fetching wishlist items:', error);
-        return [];
-      });
+  async function getWishlistItems() {
+    const response = await fetch(wishlistUrl);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch wishlist items (${response.status})`);
+    }
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const wishlistElements = doc.querySelectorAll('.mb-1');
+
+    // the first mb-1 element is the nst time, hence the slice(1)
+    return Array.from(wishlistElements).map(elem => elem.textContent.trim()).slice(1);
   }
 
   async function updateWishlistStorage() {
     try {
       const wishlistItems = await getWishlistItems();
       const wishlistString = JSON.stringify(wishlistItems);
-      localStorage.setItem('wishlist', wishlistString);
+      await GM.setValue('wishlist', wishlistString);
     } catch (error) {
       console.error('Error updating wishlist storage:', error);
     }
   }
 
-  const username = document.querySelector('#username').textContent.trim();
-  const wishlistUserUrl = `https://virtu.pet/wishlist/${username}`;
+  const wishlistStorage = JSON.parse(await GM.getValue('wishlist', '[]'));
 
-  if (localStorage.getItem('wishlist') == null || location.href == wishlistUserUrl) {
-    updateWishlistStorage();
+  if (wishlistStorage.length === 0 || location.href === wishlistUrl) {
+    await updateWishlistStorage();
   }
 
-  var wishlistStorage = JSON.parse(localStorage.getItem('wishlist'));
+  const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 
-  var textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-
-  for (var i = 0; i < textNodes.snapshotLength; i++) {
-    var node = textNodes.snapshotItem(i);
-    var text = node.textContent.trim();
+  for (let i = 0; i < textNodes.snapshotLength; i++) {
+    const node = textNodes.snapshotItem(i);
+    const text = node.textContent.trim();
 
     if (wishlistStorage.includes(text)) {
-      var span = document.createElement("span");
+      const span = document.createElement("span");
       span.style.backgroundColor = "#000000";
       span.style.color = "#44FF44";
-      var highlightedText = document.createTextNode(text);
+      const highlightedText = document.createTextNode(text);
       span.appendChild(highlightedText);
       node.parentNode.replaceChild(span, node);
     }
   }
 })();
-
-
-
-
